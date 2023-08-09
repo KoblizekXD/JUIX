d5d63980a3f42bfb6348d3dd9969460e955e81dc
KoblizekXD <janprokupek@centrum.cz>
2023-08-09 19:03:09 +0200

    Made EventManager#invoke static for easier use

--- src/main/java/lol/koblizek/juix/api/event/EventManager.java
+++ src/main/java/lol/koblizek/juix/api/event/EventManager.java
@@ -4,23 +4,21 @@ import lol.koblizek.juix.api.internal.Internal;
 import lol.koblizek.juix.core.reflect.Reflection;
 import lombok.extern.log4j.Log4j2;
 
-import java.lang.invoke.MethodHandles;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Log4j2
 public final class EventManager {
 
-    public EventManager() {
-        Internal.check();
-    }
-    public void invoke(Class<? extends Event> e, Object ... params) {
+    private EventManager() {}
+
+    public static void invoke(Class<? extends Event> e) {
         var responses = Reflection.REFLECTIONS.getMethodsWithParameter(e);
         for (Method response : responses) {
             try {
                 response.trySetAccessible();
-                response.invoke(null, params);
-            } catch (IllegalAccessException | InvocationTargetException ex) {
+                response.invoke(null, e.newInstance());
+            } catch (IllegalAccessException | InvocationTargetException | InstantiationException ex) {
                 log.error("Error invoking response method: {}", ex.getMessage());
             }
         }
