ae52b634faaf4e8b137aa7244f13f882445f8757
KoblizekXD <janprokupek@centrum.cz>
2023-08-09 08:19:36 +0200

    Added #set and #check method

--- src/main/java/lol/koblizek/juix/api/internal/Internal.java
+++ src/main/java/lol/koblizek/juix/api/internal/Internal.java
@@ -3,7 +3,7 @@ package lol.koblizek.juix.api.internal;
 import lol.koblizek.juix.core.error.UnsatisfiedInternalAccessException;
 
 public class Internal<T> {
-    private final T value;
+    private T value;
 
     public Internal(T value) {
         this.value = value;
@@ -15,5 +15,20 @@ public class Internal<T> {
             throw new UnsatisfiedInternalAccessException();
         }
         return value;
+    }
+    public void set(T val) {
+        var trace = new Throwable().getStackTrace();
+        var element = trace[trace.length-1];
+        if (!element.getClassName().startsWith("lol.koblizek.juix")) {
+            throw new UnsatisfiedInternalAccessException();
+        }
+        value = val;
+    }
+    public static void check() {
+        var trace = new Throwable().getStackTrace();
+        var element = trace[trace.length-1];
+        if (!element.getClassName().startsWith("lol.koblizek.juix")) {
+            throw new UnsatisfiedInternalAccessException();
+        }
     }
 }
