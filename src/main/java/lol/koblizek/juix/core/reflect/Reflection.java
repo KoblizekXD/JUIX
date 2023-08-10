3cc7473563c1d0f0e7256486cb9dcdb5115ba39f
KoblizekXD <janprokupek@centrum.cz>
2023-08-10 17:22:04 +0200

    Removed potentially unused code by Reflection

--- src/main/java/lol/koblizek/juix/core/reflect/Reflection.java
+++ src/main/java/lol/koblizek/juix/core/reflect/Reflection.java
@@ -16,51 +16,12 @@ import java.util.Set;
 public final class Reflection {
     public static final Reflections REFLECTIONS
             = new Reflections();
-    private final Class<?> type;
-    @Getter
-    private Object instance;
 
-    private Reflection(Class<?> type) {
-        this.type = type;
-    }
-
-    public static Set<Class<? extends Application>> getApplicationClasses() {
-        return REFLECTIONS.getSubTypesOf(Application.class);
-    }
-    public static Reflection of(Class<?> type) {
-        return new Reflection(type);
-    }
-
-    public Method getMethod(String name, Class<?> ... params) {
-        try {
-            return type.getDeclaredMethod(name, params);
-        } catch (NoSuchMethodException e) {
-            log.error("Failed to get declared method: {}, null will be returned", name);
-            return null;
-        }
-    }
-    public void setNewInstance() {
-        try {
-            instance = type.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.error("Failed to instantiate type: {}", e.getMessage());
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public Class<? extends IDisposable> getType() {
-        log.warn("getType may produce errors if used incorrectly");
-        return (Class<? extends IDisposable>) ((ParameterizedType) getClass()
-                .getGenericSuperclass()).getActualTypeArguments()[0];
-    }
     @SuppressWarnings("unchecked")
     public static Class<? extends IDisposable> getType(Application<?> application) {
         log.warn("getType may produce errors if used incorrectly");
         return (Class<? extends IDisposable>) ((ParameterizedType) application.getClass()
                 .getGenericSuperclass()).getActualTypeArguments()[0];
-    }
-    public <T extends IDisposable> Application<T> getAsApplication() {
-        return (Application<T>) getInstance();
     }
 
     public static Object newInstance(Class<?> type, Object... objects) {
